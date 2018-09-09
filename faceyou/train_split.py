4e1d73f05d57fcbf62bb4d8fdc87a3d1116458fe
阿金 <sunshinedlg@163.com>
2018-09-09 17:00:09 +0800

    增加kNN相关注释

--- faceyou/train_split.py
+++ faceyou/train_split.py
@@ -5,7 +5,7 @@
 import numpy as np
 
 
-def spilt(set_x, set_y, ratio=0.2, seed=None):
+def split(set_x, set_y, ratio=0.2, seed=None):
     assert set_x.shape[0] == set_y.shape[0], \
         'the axis of set_x must be equal to that of set_y'
     assert 0.0 <= ratio <= 1.0, \
@@ -14,14 +14,16 @@ def spilt(set_x, set_y, ratio=0.2, seed=None):
     if seed:
         np.random.seed(seed)
 
-    # 获取随机索引
+    # 根据集合数量获取N个取值为[0, N-1]的随机数
     rand_index = np.random.permutation(len(set_x))
-    # 计算测试数量
+    # 计算测试集数量
     num = int(len(set_x) * ratio)
 
+    # 确定索引，此时索引是随机的
     test_index = rand_index[:num]
     train_index = rand_index[num:]
 
+    # 获取训练集和测试集，因为索引的随机性，保证了所分配集合的随机性
     train_x = set_x[train_index]
     train_y = set_y[train_index]
     test_x = set_x[test_index]
