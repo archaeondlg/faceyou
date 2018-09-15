f61f6a9a69100849aadc13e0c92d88f45e8fd121
阿金 <sunshinedlg@163.com>
2018-09-16 01:17:23 +0800

    logistics

---
+++ faceyou/logistic.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time    : 9/15/18 3:43 PM
+
+from numpy import *
+import matplotlib.pyplot as plt
+
+
+def loadDataSet():
+    """
+    加载数据集
+    :return:输入向量矩阵和输出向量
+    """
+    dataMat = []
+    labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0, float(lineArr[0]), float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat, labelMat
+
+
+def plotBestFit(weights):
+    """
+    画出数据集和逻辑斯蒂最佳回归直线
+    :param weights:
+    :return:
+    """
+    dataMat, labelMat = loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []
+    ycord1 = []
+    xcord2 = []
+    ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i, 1])
+            ycord1.append(dataArr[i, 2])
+        else:
+            xcord2.append(dataArr[i, 1])
+            ycord2.append(dataArr[i, 2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+    ax.scatter(xcord2, ycord2, s=30, c='green')
+    if weights is not None:
+        x = arange(-3.0, 3.0, 0.1)
+        y = (-weights[0] - weights[1] * x) / weights[2]
+        ax.plot(x, y)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
+
+def sigmoid(inX):
+    return 1.0 / (1 + exp(-inX))
+
+
+def gradAscent(dataMatIn, classLabels):
+    """
+    逻辑斯蒂回归梯度上升优化suanfa
+    :param dataMatIn: 输入x矩阵
+    :param classLabels: 输出Y矩阵（类别标签组成的向量）
+    :return:
+    """
+    # 转换为numpy矩阵数组类型
+    dataMatrix = mat(dataMatIn)
+    # 转换类型，并转置
+    labelMat = mat(classLabels).transpose()
+    m, n = shape(dataMatrix)
+    alpha = 0.001
+    maxCycles = 500
+    weights = ones((n, 1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix * weights)
+        error = (labelMat - h)
+        weights += alpha * dataMatrix.transpose() * error
+    return weights
+
+
+if __name__ == '__main__':
+    dataArr, labelMat = loadDataSet()
+    weights = gradAscent(dataArr, labelMat)
+    plotBestFit(weights)
